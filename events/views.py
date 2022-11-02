0258dedd979f83f5d1aebb14fa6aaaa9cdbb169d
ieku <chulongge@gmail.com>
2022-11-03 02:16:10 +1000

    add search to view.py

--- events/views.py
+++ events/views.py
@@ -16,3 +16,13 @@ def index():
         asc(Event.date)).limit(2)
     return render_template('index.html', exhibition=exhibition, networking=networking, recreation=recreation)
 
+@mainbp.route('/search')
+def search():
+    if request.args['search']:
+        e = "%" + request.args['search'] + '%'
+        exhibition = Event.query.filter(Event.category == 'Exhibition', Event.description.like(e)).all()
+        networking = Event.query.filter(Event.category == 'Networking', Event.description.like(e)).all()
+        recreation = Event.query.filter(Event.category == 'Recreation', Event.description.like(e)).all()
+        return render_template('index.html', exhibition=exhibition, networking=networking, recreation=recreation)
+    else:
+        return redirect(url_for('main.index'))
